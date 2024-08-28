f2f308dcb7860e71f4382cc402107ac0b5aafc7c
szidzse <gellertsos@gmail.com>
2024-08-28 13:59:03 +0200

    create WebSecurityConfig

--- backend/src/main/java/com/codecool/szidzse/solarwatch/model/entity/RoleType.java
+++ backend/src/main/java/com/codecool/szidzse/solarwatch/model/entity/RoleType.java
@@ -3,5 +3,4 @@ package com.codecool.szidzse.solarwatch.model.entity;
 public enum RoleType {
     ROLE_USER,
     ROLE_ADMIN,
-    ROLE_TEAM_MEMBER
 }
---
+++ backend/src/main/java/com/codecool/szidzse/solarwatch/security/config/WebSecurityConfig.java
@@ -0,0 +1,81 @@
+package com.codecool.szidzse.solarwatch.security.config;
+
+import com.codecool.szidzse.solarwatch.security.jwt.AuthEntryPointJwt;
+import com.codecool.szidzse.solarwatch.security.jwt.AuthTokenFilter;
+import com.codecool.szidzse.solarwatch.security.jwt.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    private final UserDetailsService userDetailsService;
+
+    private final AuthEntryPointJwt unauthorizedHandler;
+
+    private final JwtUtils jwtUtils;
+
+    @Autowired
+    public WebSecurityConfig(UserDetailsService userDetailsService, AuthEntryPointJwt unauthorizedHandler, JwtUtils jwtUtils) {
+        this.userDetailsService = userDetailsService;
+        this.unauthorizedHandler = unauthorizedHandler;
+        this.jwtUtils = jwtUtils;
+    }
+
+    public AuthTokenFilter authenticationJwtTokenFilter() {
+        return new AuthTokenFilter(jwtUtils, userDetailsService);
+    }
+
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()).cors(cors -> cors.disable())
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/api/member/**").permitAll()
+                                .requestMatchers("/api/sunrise-sunset/**").hasRole("USER")
+                                .requestMatchers("/api/cities/**").hasRole("USER")
+                                .requestMatchers("/error").permitAll()
+                                .anyRequest().authenticated()
+
+                );
+
+        http.authenticationProvider(authenticationProvider());
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
