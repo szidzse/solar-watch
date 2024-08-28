795cbf03fcda71198810090ee2a26026f37324e8
szidzse <gellertsos@gmail.com>
2024-08-29 00:49:18 +0200

    refactor controller getAllCities method and create CityDTO

--- backend/src/main/java/com/codecool/szidzse/solarwatch/controller/CityController.java
+++ backend/src/main/java/com/codecool/szidzse/solarwatch/controller/CityController.java
@@ -1,5 +1,6 @@
 package com.codecool.szidzse.solarwatch.controller;
 
+import com.codecool.szidzse.solarwatch.model.DTO.CityDTO;
 import com.codecool.szidzse.solarwatch.model.entity.City;
 import com.codecool.szidzse.solarwatch.service.CityService;
 import lombok.AllArgsConstructor;
@@ -7,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/api/cities")
@@ -16,8 +18,10 @@ public class CityController {
     private final CityService cityService;
 
     @GetMapping(path = "")
-    public List<City> getAllCities() {
-        return cityService.findAll();
+    public ResponseEntity<List<CityDTO>> getAllCities() {
+        List<City> cities = cityService.findAll();
+        List<CityDTO> cityDTOs =  cities.stream().map(this::convertToDTO).toList();
+        return ResponseEntity.ok(cityDTOs);
     }
 
     @GetMapping(path = "/{id}")
@@ -41,5 +45,16 @@ public class CityController {
     public ResponseEntity<Void> deleteCity(@PathVariable Long id) {
         cityService.deleteById(id);
         return ResponseEntity.noContent().build();
+    }
+
+    private CityDTO convertToDTO(City city) {
+        CityDTO dto = new CityDTO();
+        dto.setId(city.getId());
+        dto.setName(city.getName());
+        dto.setState(city.getState());
+        dto.setCountry(city.getCountry());
+        dto.setLongitude(city.getLongitude());
+        dto.setLatitude(city.getLatitude());
+        return dto;
     }
 }
--- backend/src/main/java/com/codecool/szidzse/solarwatch/model/DTO/CityDTO.java
+++ backend/src/main/java/com/codecool/szidzse/solarwatch/model/DTO/CityDTO.java
@@ -1,4 +1,15 @@
 package com.codecool.szidzse.solarwatch.model.DTO;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class CityDTO {
+    private Long id;
+    private String name;
+    private String state;
+    private String country;
+    private BigDecimal longitude;
+    private BigDecimal latitude;
 }
