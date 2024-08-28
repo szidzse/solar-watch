0a45b8cf89df92b20ab079deb8e59ce09de5ccdc
szidzse <gellertsos@gmail.com>
2024-08-28 13:13:59 +0200

    create Member entity

---
+++ backend/src/main/java/com/codecool/szidzse/solarwatch/model/entity/Member.java
@@ -0,0 +1,36 @@
+package com.codecool.szidzse.solarwatch.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles;
+
+    public Member(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+}
