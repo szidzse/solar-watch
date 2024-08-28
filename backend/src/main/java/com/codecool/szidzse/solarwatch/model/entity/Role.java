bafd231958e6e593142ca6cbfc1b0362669534bf
szidzse <gellertsos@gmail.com>
2024-08-28 13:08:21 +0200

    create Role enetity and RoleType enum

---
+++ backend/src/main/java/com/codecool/szidzse/solarwatch/model/entity/Role.java
@@ -0,0 +1,23 @@
+package com.codecool.szidzse.solarwatch.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    public Role(RoleType roleType) {
+        this.roleType = roleType;
+    }
+}
---
+++ backend/src/main/java/com/codecool/szidzse/solarwatch/model/entity/RoleType.java
@@ -0,0 +1,7 @@
+package com.codecool.szidzse.solarwatch.model.entity;
+
+public enum RoleType {
+    ROLE_USER,
+    ROLE_ADMIN,
+    ROLE_TEAM_MEMBER
+}
